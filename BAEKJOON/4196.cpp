4532e1bfefd7a8fe4b9b23ba74d67c63f9bf45fb
얌얌이 <sonic2010@naver.com>
2020-01-25 15:29:58 +0900

    SCC

--- BAEKJOON/4196.cpp
+++ BAEKJOON/4196.cpp
@@ -18,102 +18,116 @@
 using namespace std;
 
 /*
-코드 - jaimemin.tistory.com/1279
-빠른 사람 - 이분탐색 사용 ???
+라이의 블로그 - SCC
+blog.naver.com/PostView.nhn?blogId=kks227&logNo=220802519976&categoryNo=299&parentCategoryNo=0&viewDate=&currentPage=7&postListTopCurrentPage=1&from=postList&userTopListOpen=true&userTopListCount=5&userTopListManageOpen=false&userTopListCurrentPage=7
+다른거 - 코사라주 - jason9319.tistory.com/98
+
+빠른 사람?? 
+void dfs(int i)
+{
+	if (process[i])
+		return;
+	process[i] = true;
+
+	for (auto a : direct[i])
+	{
+		dfs(a);
+	}
+	nodelist.push_back(i);
+}
+
+void dfstrue(int i)
+{
+	if (process[i])
+		return;
+	process[i] = true;
+
+	for (auto a : direct[i])
+	{
+		dfs(a);
+	}
+}
 */
 
-int n, sx, sy, hcnt;
-char arr[51][51];
-int height[51][51];
-bool visit[51][51];
-int dx[] = {0, 1, 1, 1, 0, -1, -1, -1};
-int dy[] = {1, 1, 0, -1, -1, -1, 0, 1};
+const int MAX = 100001;
+// SN: SCC 개수, sn[i]: i가 속한 SCC의 번호
+int V, E, cnt, dfsn[MAX], SN, sn[MAX];
+vector<int> adj[MAX];
+bool finished[MAX]; // SCC 분리가 끝난 정점만 true
+stack<int> S;
+vector<vector<int>> SCC;
+
+// 자신의 결과값을 리턴하는 DFS 함수
+int DFS(int curr) {
+	dfsn[curr] = ++cnt; // dfsn 결정
+	S.push(curr); // 스택에 자신을 push
+
+	// 자신의 dfsn, 자식들의 결과나 dfsn 중 가장 작은 번호를 result에 저장
+	int result = dfsn[curr];
+	for (int next : adj[curr]) {
+		// 아직 방문하지 않은 이웃
+		if (dfsn[next] == 0) result = min(result, DFS(next));
+		// 방문은 했으나 아직 SCC로 추출되지는 않은 이웃
+		else if (!finished[next]) result = min(result, dfsn[next]);
+	}
+
+	// 자신, 자신의 자손들이 도달 가능한 제일 높은 정점이 자신일 경우 SCC 추출
+	if (result == dfsn[curr]) {
+		vector<int> currSCC;
+		// 스택에서 자신이 나올 때까지 pop
+		while (1) {
+			int t = S.top();
+			S.pop();
+			currSCC.push_back(t);
+			finished[t] = true;
+			sn[t] = SN;
+			if (t == curr) break;
+		}
+		// 출력을 위해 원소 정렬
+		sort(currSCC.begin(), currSCC.end());
+		// SCC 추출
+		SCC.push_back(currSCC);
+		SN++;
+	}
+	return result;
+}
 
 int main(void)
 {
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL);
 
-	cin >> n;
-	for (int i = 0; i < n; ++i) {
-		cin >> arr[i];
-		for (int j = 0; j < n; ++j) {
-			if (arr[i][j] == 'P') {
-				sx = i;
-				sy = j;
-			}
-			else if (arr[i][j] == 'K') {
-				++hcnt;
-			}
+	int t, n, m;
+	cin >> t;
+	while (t--) { 
+		cin >> n >> m;
+		for (int i = 1; i <= n; i++)
+			adj[i].clear();
+
+		for (int x, y, i = 0; i < m; ++i) {
+			cin >> x >> y;
+			adj[x].push_back(y);
 		}
+
+		cnt = SN = 0;
+		memset(dfsn, 0, sizeof(dfsn));
+		memset(sn, 0, sizeof(sn));
+		memset(finished, 0, sizeof(finished));
+		for (int i = 1; i <= n; i++)
+			if (dfsn[i] == 0) DFS(i);
+		
+		// SCC의 indegree를 셈
+		int sOutdegree[MAX] = { 0, };
+		for (int i = 1; i <= n; i++)
+			// 간선 i->j에 대해, i와 j가 다른 SCC에 속하면 j가 속한 SCC의 outdegree++
+			for (int j : adj[i])
+				if (sn[i] != sn[j]) sOutdegree[sn[j]]++;
+
+		int result = 0;
+		for (int i = 0; i < SN; i++)
+			if (sOutdegree[i] == 0) result++;
+		cout << result << '\n';
 	}
-
-	vector<int> fatigue;
-	for (int i = 0; i < n; ++i) {
-		for (int j = 0; j < n; ++j) {
-			cin >> height[i][j];
-			fatigue.push_back(height[i][j]);
-		}
-	}
-
-	sort(fatigue.begin(), fatigue.end());
-	fatigue.erase(unique(fatigue.begin(), fatigue.end()), fatigue.end());
-
-	int ans = 1000001;
-	int low = 0, high = 0; // 투 포인터
-
-	while (low < fatigue.size()) {
-		memset(visit, false, sizeof(visit));
-
-		queue<pair<int, int>> q;
-		int piro = height[sx][sy];
-
-		if (piro >= fatigue[low] && piro <= fatigue[high]) {
-			visit[sx][sy] = true;
-			q.push({ sx, sy });
-		}
-
-		int K = 0;
-		while (!q.empty()) {
-			int y = q.front().first;
-			int x = q.front().second;
-			q.pop();
-
-			for (int k = 0; k < 8; k++) {
-				int nextY = y + dy[k];
-				int nextX = x + dx[k];
-
-				if (nextY < 0 || nextY >= n || nextX < 0 || nextX >= n || visit[nextY][nextX])
-				{
-					continue;
-				}
-
-				int piro = height[nextY][nextX];
-
-				if (piro >= fatigue[low] && piro <= fatigue[high]) {
-					visit[nextY][nextX] = true;
-					q.push({ nextY, nextX });
-
-					if (arr[nextY][nextX] == 'K') {
-						K++;
-					}
-				}
-			}
-		}
-
-		if (hcnt == K) { // 범위 좁힘
-			ans = min(ans, fatigue[high] - fatigue[low]);
-			low++;
-		}
-		else if (high + 1 < fatigue.size()) { // 범위 넓힘
-			high++;
-		}
-		else {
-			break;
-		}
-	}
-
-	cout << ans << "\n";
 
 	return 0;
 }
